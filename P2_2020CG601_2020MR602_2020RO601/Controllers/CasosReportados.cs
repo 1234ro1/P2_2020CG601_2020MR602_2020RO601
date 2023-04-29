0b5c7b626c913a64d72615b753c4a8e129b56de9
CarlosD <carlos.chinchilla1@catolica.edu.sv>
2023-04-29 09:21:28 -0600

    .

---
+++ P2_2020CG601_2020MR602_2020RO601/Controllers/CasosReportados.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using P2_2020CG601_2020MR602_2020RO601.Models;
+
+namespace P2_2020CG601_2020MR602_2020RO601.Controllers
+{
+    public class CasosReportados : Controller
+    {
+        private readonly covidContext _covidContext;
+        public CasosReportados(covidContext covidContext)
+        {
+            _covidContext= covidContext;
+        }
+        public IActionResult indexx() 
+        {
+            var listadoDepartamentos = (from d in _covidContext.Departamentos select d).ToList();
+            ViewData["listadoDepartamentos"] = new SelectList(listadoDepartamentos, "id_departamento", "nombre_departamento");
+
+            var listadoDeGeneros = (from g in _covidContext.Generos select g).ToList();
+            ViewData["listadoDeGeneros"] = new SelectList(listadoDeGeneros, "id_genero", "nombre_genero");
+            
+            return View();
+        }
+    }
+}
